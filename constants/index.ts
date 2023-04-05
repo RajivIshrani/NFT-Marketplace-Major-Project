0030869bf75ec9dba2071a6ac8a5b1187444ef2e
Ishrani Rajiv <rajivishrani@gmail.com>
2023-04-05 21:35:00 +0530

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,9 @@
+// Address of the RewardToken Contract that you deployed
+const REWARDTOKEN_CONTRACT_ADDRESS =
+    "0x404fdEd7112Bb4d046283725101eaf8521cB0be6"
+
+// Address of the StakingToken Contract that you deployed
+const STAKINGTOKEN_CONTRACT_ADDRESS =
+    "0xda537F79eBda6973D7b5bC4e68C8fe22CEBBeDB1"
+
+export { REWARDTOKEN_CONTRACT_ADDRESS, STAKINGTOKEN_CONTRACT_ADDRESS }
